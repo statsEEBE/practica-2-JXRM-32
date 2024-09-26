b96811f283da92b5089929470801cd7f9af4a0c0
josep.xavier.ricart <josep.xavier.ricart@auleseebe.upc.edu>
2024-09-26 11:29:03 +0200

    Pràctica 2 Problema 1 resolt

--- Problema1.R
+++ Problema1.R
@@ -1,2 +1,11 @@
-#Codigo para problema 1
+# Codigo para problema 1
+# Mirant les dades determinem el coefiient de determinacio Rsq
+# i calculem el coeficient de correlació
+Rsq <- 0.1923
+Coef_Cor2 <- sqrt(Rsq)
 
+# Mirant les dades construim y = m*x + b i calculem y de 647.1
+m <- 0.06576
+b <- 11.79755
+m*647.1+b
+ypred <- predict(m,b, data.frame(x=x))
